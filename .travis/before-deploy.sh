64b1c84f44467a7af9b4125190a6f8b398f89295
nelt <nel.taurisson@gmail.com>
2017-04-24 05:36:46 +0000

    travis before-deploy fix

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_544db893dbcc_key -iv $encrypted_544db893dbcc_iv -in codesigning.asc.enc -out codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_544db893dbcc_key -iv $encrypted_544db893dbcc_iv -in .travis/codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import codesigning.asc
 fi
