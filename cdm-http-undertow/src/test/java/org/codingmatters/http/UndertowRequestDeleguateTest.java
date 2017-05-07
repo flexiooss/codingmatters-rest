f78a37ba28d275dc5961a765432ca17a7c3f2aa9
nelt <nel.taurisson@gmail.com>
2017-05-07 12:57:26 +0000

    [undertow] added test for request deleguate

---
+++ cdm-http-undertow/src/test/java/org/codingmatters/http/UndertowRequestDeleguateTest.java
@@ -0,0 +1,164 @@
+package org.codingmatters.http;
+
+import io.undertow.Undertow;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import org.codingmatters.http.api.Processor;
+import org.codingmatters.http.api.RequestDeleguate;
+import org.codingmatters.http.api.ResponseDeleguate;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.ServerSocket;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Matcher;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by nelt on 5/7/17.
+ */
+public class UndertowRequestDeleguateTest {
+
+    private Undertow server;
+    private OkHttpClient client = new OkHttpClient();
+    private String baseUrl;
+    private Processor testProcessor;
+
+    @Before
+    public void setUp() throws Exception {
+        int port;
+        try(ServerSocket socket = new ServerSocket(0)) {
+            port = socket.getLocalPort();
+        }
+        this.server = Undertow.builder()
+                .addHttpListener(port, "localhost")
+                .setHandler(new CdmHttpUndertowHandler(this::process))
+                .build();
+        this.server.start();
+
+        this.baseUrl = "http://localhost:" + port;
+    }
+
+    private void process(RequestDeleguate requestDeleguate, ResponseDeleguate responseDeleguate) throws IOException {
+        this.testProcessor.process(requestDeleguate, responseDeleguate);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.server.stop();
+    }
+
+    @Test
+    public void method_get() throws Exception {
+        AtomicReference<RequestDeleguate.Method> method = new AtomicReference<>();
+        this.testProcessor = (requestDeleguate, responseDeleguate) -> {method.set(requestDeleguate.method());};
+
+        this.client.newCall(this.requestBuilder().get().build()).execute();
+        assertThat(method.get(), is(RequestDeleguate.Method.GET));
+    }
+
+    @Test
+    public void method_post() throws Exception {
+        AtomicReference<RequestDeleguate.Method> method = new AtomicReference<>();
+        this.testProcessor = (requestDeleguate, responseDeleguate) -> {method.set(requestDeleguate.method());};
+
+        this.client.newCall(this.requestBuilder().post(this.emptyJsonBody()).build()).execute();
+        assertThat(method.get(), is(RequestDeleguate.Method.POST));
+    }
+
+    @Test
+    public void method_put() throws Exception {
+        AtomicReference<RequestDeleguate.Method> method = new AtomicReference<>();
+        this.testProcessor = (requestDeleguate, responseDeleguate) -> {method.set(requestDeleguate.method());};
+
+        this.client.newCall(this.requestBuilder().put(this.emptyJsonBody()).build()).execute();
+        assertThat(method.get(), is(RequestDeleguate.Method.PUT));
+    }
+
+    @Test
+    public void method_patch() throws Exception {
+        AtomicReference<RequestDeleguate.Method> method = new AtomicReference<>();
+        this.testProcessor = (requestDeleguate, responseDeleguate) -> {method.set(requestDeleguate.method());};
+
+        this.client.newCall(this.requestBuilder().patch(this.emptyJsonBody()).build()).execute();
+        assertThat(method.get(), is(RequestDeleguate.Method.PATCH));
+    }
+
+    @Test
+    public void method_delete() throws Exception {
+        AtomicReference<RequestDeleguate.Method> method = new AtomicReference<>();
+        this.testProcessor = (requestDeleguate, responseDeleguate) -> {method.set(requestDeleguate.method());};
+
+        this.client.newCall(this.requestBuilder().delete().build()).execute();
+        assertThat(method.get(), is(RequestDeleguate.Method.DELETE));
+    }
+
+    @Test
+    public void pathMatcher() throws Exception {
+        AtomicReference<Matcher> matcher = new AtomicReference<>();
+        this.testProcessor = (requestDeleguate, responseDeleguate) -> {matcher.set(requestDeleguate.pathMatcher("/[a]+/[b]+"));};
+
+        this.client.newCall(this.requestBuilder("/aaa/bbb").get().build()).execute();
+        assertThat(matcher.get().matches(), is(true));
+    }
+
+    @Test
+    public void absolutePath() throws Exception {
+        AtomicReference<String> absolutePath = new AtomicReference<>();
+        this.testProcessor = (requestDeleguate, responseDeleguate) -> {absolutePath.set(requestDeleguate.absolutePath("aaa/bbb"));};
+
+        this.client.newCall(this.requestBuilder().get().build()).execute();
+        assertThat(absolutePath.get(), is(this.baseUrl + "/aaa/bbb"));
+    }
+
+    @Test
+    public void payload() throws Exception {
+        AtomicReference<String> payload = new AtomicReference<>();
+        this.testProcessor = (requestDeleguate, responseDeleguate) -> {
+            String result = this.readAsString(requestDeleguate);
+            payload.set(result);
+        };
+
+        String requestPayload = "{\"yop\":\"yop\"}";
+        this.client.newCall(this.requestBuilder().post(this.jsonBody(requestPayload)).build()).execute();
+        assertThat(payload.get(), is(requestPayload));
+    }
+
+    private Request.Builder requestBuilder() {
+        return this.requestBuilder("");
+    }
+
+    private Request.Builder requestBuilder(String path) {
+        return new Request.Builder()
+                .url(this.baseUrl + path);
+    }
+
+    private RequestBody emptyJsonBody() {
+        String payload = "{}";
+        return jsonBody(payload);
+    }
+
+    private RequestBody jsonBody(String payload) {
+        return RequestBody.create(MediaType.parse("application/json"), payload);
+    }
+
+    private String readAsString(RequestDeleguate requestDeleguate) throws IOException {
+        StringBuilder payloadAsString = new StringBuilder();
+        try(InputStream in = requestDeleguate.payload(); Reader reader = new InputStreamReader(in)) {
+            char [] buffer = new char[1024];
+            for(int read = reader.read(buffer) ; read != -1 ; read = reader.read(buffer)) {
+                payloadAsString.append(buffer, 0, read);
+            }
+        }
+        return payloadAsString.toString();
+    }
+}
