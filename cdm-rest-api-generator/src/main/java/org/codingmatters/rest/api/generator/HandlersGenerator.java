f6ff83be4345379f29d757fe51d1402c8a1fab96
nelt <nel.taurisson@gmail.com>
2017-05-16 04:40:19 +0000

    [api-generator] handlers interface

--- cdm-rest-api-generator/src/main/java/org/codingmatters/rest/api/generator/HandlersGenerator.java
+++ cdm-rest-api-generator/src/main/java/org/codingmatters/rest/api/generator/HandlersGenerator.java
@@ -1,36 +1,75 @@
 package org.codingmatters.rest.api.generator;
 
-import org.codingmatters.rest.api.generator.exception.RamlSpecException;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeSpec;
 import org.codingmatters.rest.api.generator.utils.Naming;
-import org.codingmatters.value.objects.spec.Spec;
 import org.raml.v2.api.RamlModelResult;
+import org.raml.v2.api.model.v10.methods.Method;
 import org.raml.v2.api.model.v10.resources.Resource;
 
+import javax.lang.model.element.Modifier;
 import java.io.File;
+import java.io.IOException;
+import java.util.function.Function;
+
+import static org.codingmatters.value.objects.generation.GenerationUtils.packageDir;
+import static org.codingmatters.value.objects.generation.GenerationUtils.writeJavaFile;
 
 /**
  * Created by nelt on 5/15/17.
  */
 public class HandlersGenerator {
 
+    private final String serverPackage;
     private final String typesPackage;
     private final String apiPackage;
     private final Naming naming = new Naming();
     private final File rootDirectory;
 
-    public HandlersGenerator(String typesPackage, String apiPackage, File toDirectory) {
+    public HandlersGenerator(String serverPackage, String typesPackage, String apiPackage, File toDirectory) {
+        this.serverPackage = serverPackage;
         this.typesPackage = typesPackage;
         this.apiPackage = apiPackage;
         this.rootDirectory = toDirectory;
     }
 
-    public Spec generate(RamlModelResult ramlModel) throws RamlSpecException {
-        Spec.Builder result = Spec.spec();
+    public void generate(RamlModelResult ramlModel) throws IOException {
+        TypeSpec handlersInterface = this.createHandlersInterface(ramlModel)
+                .build();
+
+        writeJavaFile(
+                packageDir(this.rootDirectory, this.serverPackage),
+                this.serverPackage,
+                handlersInterface);
+    }
+
+    private TypeSpec.Builder createHandlersInterface(RamlModelResult ramlModel) {
+        TypeSpec.Builder result = TypeSpec.interfaceBuilder(this.handlersInterfaceName(ramlModel))
+                .addModifiers(Modifier.STATIC, Modifier.PUBLIC);
+
         for (Resource resource : ramlModel.getApiV10().resources()) {
-//            this.generateResourceValues(result, resource);
+            String resourceName = resource.displayName().value();
+            for (Method method : resource.methods()) {
+                String methodName = method.method();
+                result.addMethod(MethodSpec.methodBuilder(this.naming.property(resourceName, methodName, "Handler"))
+                        .addModifiers(Modifier.ABSTRACT, Modifier.PUBLIC)
+                        .returns(ParameterizedTypeName.get(
+                                ClassName.get(Function.class),
+                                ClassName.get(this.apiPackage, this.naming.type(resourceName, methodName, "Request")),
+                                ClassName.get(this.apiPackage, this.naming.type(resourceName, methodName, "Response"))
+                                )
+                        )
+                        .build());
+            }
         }
 
-        return result.build();
+        return result;
+    }
+
+    private String handlersInterfaceName(RamlModelResult ramlModel) {
+        return this.naming.type(ramlModel.getApiV10().title().value(), "Handlers");
     }
 
 }
--- cdm-rest-api-generator/src/test/java/org/codingmatters/rest/api/generator/HandlersGeneratorTest.java
+++ cdm-rest-api-generator/src/test/java/org/codingmatters/rest/api/generator/HandlersGeneratorTest.java
@@ -11,13 +11,22 @@ import org.junit.rules.TemporaryFolder;
 import org.raml.v2.api.RamlModelBuilder;
 import org.raml.v2.api.RamlModelResult;
 
+import java.io.*;
+import java.util.function.Function;
+
+import static org.codingmatters.tests.reflect.ReflectMatchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by nelt on 5/15/17.
  */
 public class HandlersGeneratorTest {
 
-    public static final String TYPES_PACK = "org.generated.types";
-    public static final String API_PACK = "org.generated.api";
+    public static final String ROOT_PACK = "org.generated";
+    public static final String TYPES_PACK = ROOT_PACK + ".types";
+    public static final String API_PACK = ROOT_PACK + ".api";
+    public static final String SERVER_PACK = ROOT_PACK + ".server";
 
     @Rule
     public TemporaryFolder dir = new TemporaryFolder();
@@ -32,13 +41,71 @@ public class HandlersGeneratorTest {
         Spec apiSpec = new ApiGenerator(TYPES_PACK).generate(raml);
         new SpecCodeGenerator(apiSpec, API_PACK, this.dir.getRoot()).generate();
 
-        new HandlersGenerator(API_PACK, TYPES_PACK, this.dir.getRoot()).generate(raml);
+        new HandlersGenerator(SERVER_PACK, TYPES_PACK, API_PACK, this.dir.getRoot()).generate(raml);
 
         this.compiled = CompiledCode.builder().source(this.dir.getRoot()).compile();
+
+        this.printContent("", this.dir.getRoot());
     }
 
     @Test
-    public void name() throws Exception {
+    public void interface_() throws Exception {
+        this.printFile(this.dir.getRoot(), "TestAPIHandlers.java");
 
+        assertThat(
+            this.compiled.getClass(SERVER_PACK + ".TestAPIHandlers"),
+                is(aPublic().interface_()
+                        .with(aPublic().method()
+                                .named("rootGetHandler")
+                                .returning(genericType()
+                                        .baseClass(Function.class)
+                                        .withParameters(
+                                                classTypeParameter(this.compiled.getClass(API_PACK + ".RootGetRequest")),
+                                                classTypeParameter(this.compiled.getClass(API_PACK + ".RootGetResponse"))
+                                        ))
+                        )
+                        .with(aPublic().method()
+                                .named("rootPostHandler")
+                                .returning(genericType()
+                                        .baseClass(Function.class)
+                                        .withParameters(
+                                                classTypeParameter(this.compiled.getClass(API_PACK + ".RootPostRequest")),
+                                                classTypeParameter(this.compiled.getClass(API_PACK + ".RootPostResponse"))
+                                        ))
+                        )
+
+                )
+        );
+    }
+
+    private void printContent(String prefix, File root) {
+        if(root.isDirectory()) {
+            System.out.println(prefix + " + " + root.getName());
+            for (File file : root.listFiles()) {
+                this.printContent(prefix + "   ", file);
+            }
+
+        } else if(root.getName().endsWith(".java")){
+            System.out.println(prefix + "   " + root.getName());
+        }
+    }
+
+    private void printFile(File root, String name) throws IOException {
+        if(root.getName().equals(name)) {
+            System.out.println("FILE CONTENT - " + root.getAbsolutePath());
+            try(InputStream in = new FileInputStream(root) ; Reader reader = new InputStreamReader(in)) {
+                char [] buffer = new char[1024];
+                StringBuilder content = new StringBuilder();
+                for(int read  = reader.read(buffer) ; read != -1 ; read = reader.read(buffer)) {
+                    content.append(buffer, 0, read);
+                }
+                System.out.println(content);
+            }
+            System.out.println("--------------------------------");
+        } else if(root.listFiles() != null) {
+            for (File file : root.listFiles()) {
+                this.printFile(file, name);
+            }
+        }
     }
 }
