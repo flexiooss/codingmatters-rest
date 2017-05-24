b29b9c0a00cfd77cbe1fc84576bf3853896643fe
nelt <nel.taurisson@gmail.com>
2017-05-24 04:44:40 +0000

    using CompiledCode.findLibraryInClasspath instead of raw pattern lookup

--- cdm-rest-api-generator/src/main/java/org/codingmatters/rest/api/generator/ProcessorGenerator.java
+++ cdm-rest-api-generator/src/main/java/org/codingmatters/rest/api/generator/ProcessorGenerator.java
@@ -1,8 +1,42 @@
 package org.codingmatters.rest.api.generator;
 
+import com.squareup.javapoet.TypeSpec;
+import org.codingmatters.rest.api.generator.handlers.HandlersHelper;
+import org.codingmatters.rest.api.generator.processors.ProcessorClass;
+import org.codingmatters.rest.api.generator.utils.Naming;
+import org.raml.v2.api.RamlModelResult;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.codingmatters.value.objects.generation.GenerationUtils.packageDir;
+import static org.codingmatters.value.objects.generation.GenerationUtils.writeJavaFile;
+
 /**
  * Created by nelt on 5/15/17.
  */
 public class ProcessorGenerator {
+    private final String serverPackage;
+    private final String typesPackage;
+    private final String apiPackage;
+    private final Naming naming;
+    private final File rootDirectory;
+    private final HandlersHelper helper;
 
+    public ProcessorGenerator(String serverPackage, String typesPackage, String apiPackage, File toDirectory) {
+        this.serverPackage = serverPackage;
+        this.typesPackage = typesPackage;
+        this.apiPackage = apiPackage;
+        this.rootDirectory = toDirectory;
+        this.naming = new Naming();
+        this.helper = new HandlersHelper(this.apiPackage, this.naming);
+    }
+
+    public void generate(RamlModelResult ramlModel) throws IOException {
+        TypeSpec processorClass = new ProcessorClass(this.typesPackage, this.apiPackage, this.naming, this.helper).type(ramlModel);
+        writeJavaFile(
+                packageDir(this.rootDirectory, this.serverPackage),
+                this.serverPackage,
+                processorClass);
+    }
 }
--- cdm-rest-api-generator/src/test/java/org/codingmatters/rest/api/generator/ProcessorGeneratorTest.java
+++ cdm-rest-api-generator/src/test/java/org/codingmatters/rest/api/generator/ProcessorGeneratorTest.java
@@ -1,14 +1,20 @@
 package org.codingmatters.rest.api.generator;
 
+import org.codingmatters.rest.api.Processor;
 import org.codingmatters.rest.api.tests.utils.FileHelper;
 import org.codingmatters.tests.compile.CompiledCode;
 import org.codingmatters.value.objects.generation.SpecCodeGenerator;
 import org.codingmatters.value.objects.spec.Spec;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.raml.v2.api.RamlModelBuilder;
 import org.raml.v2.api.RamlModelResult;
+
+import static org.codingmatters.tests.reflect.ReflectMatchers.aPublic;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by nelt on 5/23/17.
@@ -29,19 +35,32 @@ public class ProcessorGeneratorTest {
 
     @Before
     public void setUp() throws Exception {
-        RamlModelResult raml = new RamlModelBuilder().buildApi(this.fileHelper.fileResource("handlers/handlers.raml"));
+        RamlModelResult raml = new RamlModelBuilder().buildApi(this.fileHelper.fileResource("processor/processor-base.raml"));
         Spec typesSpec = new ApiTypesGenerator().generate(raml);
         new SpecCodeGenerator(typesSpec, TYPES_PACK, this.dir.getRoot()).generate();
 
         Spec apiSpec = new ApiGenerator(TYPES_PACK).generate(raml);
         new SpecCodeGenerator(apiSpec, API_PACK, this.dir.getRoot()).generate();
-
         new HandlersGenerator(SERVER_PACK, TYPES_PACK, API_PACK, this.dir.getRoot()).generate(raml);
 
-        this.compiled = CompiledCode.builder().source(this.dir.getRoot()).compile();
+        new ProcessorGenerator(SERVER_PACK, TYPES_PACK, API_PACK, this.dir.getRoot()).generate(raml);
+
+        this.compiled = CompiledCode.builder()
+                .classpath(CompiledCode.findLibraryInClasspath("cdm-rest-api"))
+                .source(this.dir.getRoot())
+                .compile();
 
         this.fileHelper.printJavaContent("", this.dir.getRoot());
-        this.fileHelper.printFile(this.dir.getRoot(), "TestAPIHandlers.java");
+        this.fileHelper.printFile(this.dir.getRoot(), "TestAPIProcessor.java");
     }
 
+    @Test
+    public void processorClass() throws Exception {
+        assertThat(
+                this.compiled.getClass("org.generated.server.TestAPIProcessor"),
+                is(
+                        aPublic().class_().implementing(Processor.class)
+                )
+        );
+    }
 }
