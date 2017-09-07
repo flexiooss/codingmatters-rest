945a2a5152d0f6c92dae1f6ecca32375b737beb5
nelt <nel.taurisson@gmail.com>
2017-09-07 04:26:39 +0000

    [cdm-rest-client] Requester is now an interface

--- cdm-rest-client-api/src/main/java/org/codingmatters/rest/api/client/Requester.java
+++ cdm-rest-client-api/src/main/java/org/codingmatters/rest/api/client/Requester.java
@@ -1,44 +1,17 @@
 package org.codingmatters.rest.api.client;
 
 import java.io.IOException;
-import java.util.TreeMap;
 
-public abstract class Requester {
+public interface Requester {
 
-    public abstract ResponseDelegate get() throws IOException;
-    public abstract ResponseDelegate post(String contentType, byte[] body) throws IOException;
-    public abstract ResponseDelegate put(String contentType, byte[] body) throws IOException;
-    public abstract ResponseDelegate patch(String contentType, byte[] body) throws IOException;
-    public abstract ResponseDelegate delete() throws IOException;
-    public abstract ResponseDelegate delete(String contentType, byte[] body) throws IOException;
+    ResponseDelegate get() throws IOException;
+    ResponseDelegate post(String contentType, byte[] body) throws IOException;
+    ResponseDelegate put(String contentType, byte[] body) throws IOException;
+    ResponseDelegate patch(String contentType, byte[] body) throws IOException;
+    ResponseDelegate delete() throws IOException;
+    ResponseDelegate delete(String contentType, byte[] body) throws IOException;
 
-    private String path = "/";
-    private final TreeMap<String, String> queryParameters = new TreeMap<>();
-    private final TreeMap<String, String> headers = new TreeMap<>();
-
-    public Requester parameter(String name, String value) {
-        this.queryParameters.put(name, value);
-        return this;
-    }
-
-    public Requester header(String name, String value) {
-        this.headers.put(name, value);
-        return this;
-    }
-
-    public Requester path(String path) {
-        this.path = path;
-        return this;
-    }
-
-    protected String path() {
-        return path;
-    }
-
-    protected TreeMap<String, String> parameters() {
-        return queryParameters;
-    }
-    protected TreeMap<String, String> headers() {
-        return headers;
-    }
+    Requester parameter(String name, String value);
+    Requester header(String name, String value);
+    Requester path(String path);
 }
--- cdm-rest-client-okhttp/src/main/java/org/codingmatters/rest/api/client/okhttp/OkHttpRequester.java
+++ cdm-rest-client-okhttp/src/main/java/org/codingmatters/rest/api/client/okhttp/OkHttpRequester.java
@@ -11,10 +11,16 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Map;
+import java.util.TreeMap;
 
-public class OkHttpRequester extends Requester {
+public class OkHttpRequester implements Requester {
     private final OkHttpClient client;
     private final String baseUrl;
+
+    private String path = "/";
+    private final TreeMap<String, String> queryParameters = new TreeMap<>();
+    private final TreeMap<String, String> headers = new TreeMap<>();
+
 
     public OkHttpRequester(OkHttpClient client, String url) {
         this.client = client;
@@ -54,6 +60,32 @@ public class OkHttpRequester extends Requester {
     public ResponseDelegate delete(String contentType, byte[] body) throws IOException {
         Request request = this.prepareRequestBuilder().delete(RequestBody.create(MediaType.parse(contentType), body)).build();
         return new OkHttpResponseDelegate(this.client.newCall(request).execute());
+    }
+
+
+    public Requester parameter(String name, String value) {
+        this.queryParameters.put(name, value);
+        return this;
+    }
+
+    public Requester header(String name, String value) {
+        this.headers.put(name, value);
+        return this;
+    }
+
+    public Requester path(String path) {
+        this.path = path;
+        return this;
+    }
+
+    protected String path() {
+        return path;
+    }
+    protected TreeMap<String, String> parameters() {
+        return queryParameters;
+    }
+    protected TreeMap<String, String> headers() {
+        return headers;
     }
 
 
