a29322e008d470b022095bb75535148aba477f06
nelt <nel.taurisson@gmail.com>
2017-09-28 20:28:56 +0000

    [client] response headers

---
+++ cdm-rest-api-generator/src/test/java/org/codingmatters/rest/api/generator/client/RequesterClientGeneratorResponseHeadersTest.java
@@ -0,0 +1,79 @@
+package org.codingmatters.rest.api.generator.client;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import org.codingmatters.rest.api.client.RequesterFactory;
+import org.codingmatters.rest.api.generator.client.support.RequesterClientTestSetup;
+import org.codingmatters.rest.api.generator.client.support.TestRequesterFactory;
+import org.codingmatters.tests.compile.FileHelper;
+import org.codingmatters.tests.compile.helpers.ClassLoaderHelper;
+import org.codingmatters.tests.compile.helpers.helpers.ObjectHelper;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TemporaryFolder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.codingmatters.rest.api.generator.client.support.ClientGeneratorHelper.API_PACK;
+import static org.codingmatters.rest.api.generator.client.support.ClientGeneratorHelper.CLIENT_PACK;
+import static org.codingmatters.tests.compile.helpers.ClassLoaderHelper.c;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class RequesterClientGeneratorResponseHeadersTest {
+    public TemporaryFolder dir = new TemporaryFolder();
+    public FileHelper fileHelper = new FileHelper();
+    public RequesterClientTestSetup support = new RequesterClientTestSetup("processor/processor-response.raml", this.dir, this.fileHelper);
+
+    @Rule
+    public RuleChain chain= RuleChain
+            .outerRule(this.dir)
+            .around(this.fileHelper)
+            .around(this.support);
+    private ClassLoaderHelper classes;
+
+    @Before
+    public void setUp() throws Exception {
+        this.fileHelper.printJavaContent("", this.dir.getRoot());
+        this.fileHelper.printFile(this.dir.getRoot(), "HeadersClient.java");
+        this.classes = this.support.compiled().classLoader();
+    }
+
+
+    @Test
+    public void headers() throws Exception {
+        TestRequesterFactory requesterFactory = new TestRequesterFactory();
+        JsonFactory jsonFactory = new JsonFactory();
+        String baseUrl = "https://path.to/me";
+
+
+        Map<String, String[]> headers = new HashMap<>();
+        headers.put("stringParam", new String [] {"value"});
+        headers.put("arrayParam", new String [] {"v1", "v2"});
+        requesterFactory.nextResponse(TestRequesterFactory.Method.GET, 200, null, headers);
+
+        ObjectHelper response = this.classes.get(CLIENT_PACK + ".TestAPIRequesterClient")
+                .newInstance(RequesterFactory.class, JsonFactory.class, String.class)
+                .with(requesterFactory, jsonFactory, baseUrl)
+                .call("headers")
+                    .call("get", c(API_PACK + ".HeadersGetRequest"))
+                    .with(
+                            this.classes.get(API_PACK + ".HeadersGetRequest")
+                                .call("builder")
+                                .call("build")
+                                .get()
+                    );
+
+        System.out.println(response.get());
+
+        ObjectHelper status200 = response.as(c(API_PACK + ".HeadersGetResponse"))
+                .call("status200").as(API_PACK + ".headersgetresponse.Status200");
+
+        assertThat(status200.call("stringParam").get(), is("value"));
+        assertThat(status200.call("arrayParam").call("get", int.class).with(0).get(), is("v1"));
+        assertThat(status200.call("arrayParam").call("get", int.class).with(1).get(), is("v2"));
+    }
+
+}
---
+++ cdm-rest-maven-plugin/src/main/java/org/codingmatters/rest/maven/plugin/GenerateAPIClientSideMojo.java
@@ -0,0 +1,46 @@
+package org.codingmatters.rest.maven.plugin;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codingmatters.rest.api.generator.ClientInterfaceGenerator;
+import org.codingmatters.rest.api.generator.ClientRequesterImplementation;
+import org.raml.v2.api.RamlModelResult;
+
+import java.io.File;
+import java.io.IOException;
+
+
+@Mojo(name = "generate-client-side")
+public class GenerateAPIClientSideMojo extends AbstractGenerateAPIMojo {
+
+    @Parameter(required = true, alias = "destination-package")
+    private String destinationPackage;
+
+    @Parameter(required = true, alias = "types-package")
+    private String typesPackage;
+
+    @Parameter(required = true, alias = "api-package")
+    private String apiPackage;
+
+    @Parameter(defaultValue = "${basedir}/target/generated-sources/")
+    private File outputDirectory;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        RamlModelResult raml = this.resolveRamlModel();
+
+        try {
+            new ClientInterfaceGenerator(this.destinationPackage, this.apiPackage, this.outputDirectory).generate(raml);
+        } catch (IOException e) {
+            throw new MojoExecutionException("error generating client interface from raml model", e);
+        }
+        try {
+            new ClientRequesterImplementation(this.destinationPackage, this.apiPackage, this.typesPackage, this.outputDirectory).generate(raml);
+        } catch (IOException e) {
+            throw new MojoExecutionException("error generating requester client implementation from raml model", e);
+        }
+
+    }
+}
