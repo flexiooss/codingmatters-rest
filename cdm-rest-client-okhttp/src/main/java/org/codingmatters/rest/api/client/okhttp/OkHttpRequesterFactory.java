e69cac64879e39c7fe118e4aa73319d69991ff20
Nel <nel@flexio.fr>
2018-05-22 15:59:11 +0200

    wrapper constructor for OkHttpRequesterFactory

--- cdm-rest-client-okhttp/src/main/java/org/codingmatters/rest/api/client/okhttp/OkHttpRequesterFactory.java
+++ cdm-rest-client-okhttp/src/main/java/org/codingmatters/rest/api/client/okhttp/OkHttpRequesterFactory.java
@@ -5,18 +5,21 @@ import org.codingmatters.rest.api.client.Requester;
 import org.codingmatters.rest.api.client.RequesterFactory;
 
 public class OkHttpRequesterFactory implements RequesterFactory {
-    private final OkHttpClient client;
+    private final OkHttpClientWrapper client;
 
     public OkHttpRequesterFactory(OkHttpClient client) {
-        this.client = client;
+        this.client = OkHttpClientWrapper.from(client);
     }
 
+    public OkHttpRequesterFactory(OkHttpClientWrapper client) {
+        this.client = client;
+    }
 
     @Override
     public Requester forBaseUrl(String url) {
         while(url.endsWith("/")) {
             url = url.substring(0, url.length() - 1);
         }
-        return new OkHttpRequester(OkHttpClientWrapper.from(this.client), url);
+        return new OkHttpRequester(this.client, url);
     }
 }
