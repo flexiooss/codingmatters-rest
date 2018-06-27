881e526bf1d646b6a1f7670044ac9e198f62fa36
Nel <nel@flexio.fr>
2018-06-27 08:48:29 +0200

    fixed env var for timeouts

--- cdm-rest-client-okhttp/src/main/java/org/codingmatters/rest/api/client/okhttp/OkHttpClientWrapper.java
+++ cdm-rest-client-okhttp/src/main/java/org/codingmatters/rest/api/client/okhttp/OkHttpClientWrapper.java
@@ -14,8 +14,8 @@ import java.util.concurrent.TimeUnit;
 public class OkHttpClientWrapper {
 
     static public final String OK_WRAPPER_CONNECT_TIMEOUT_ENV = "OK_WRAPPER_CONNECT_TIMEOUT";
-    static public final String OK_WRAPPER_READ_TIMEOUT_ENV = "OK_WRAPPER_CONNECT_TIMEOUT";
-    static public final String OK_WRAPPER_WRITE_TIMEOUT_ENV = "OK_WRAPPER_CONNECT_TIMEOUT";
+    static public final String OK_WRAPPER_READ_TIMEOUT_ENV = "OK_WRAPPER_READ_TIMEOUT";
+    static public final String OK_WRAPPER_WRITE_TIMEOUT_ENV = "OK_WRAPPER_WRITE_TIMEOUT";
 
     static public OkHttpClientWrapper build() {
         return build(new OkHttpClient.Builder()
@@ -25,10 +25,28 @@ public class OkHttpClientWrapper {
         );
     }
 
+    /**
+     * This one should only used in unit tests.
+     *
+     * Please consider using the no arg build() anv set timeout values with env / system property variables.
+     *
+     * @param builder
+     * @return
+     */
+    @Deprecated
     static public OkHttpClientWrapper build(OkHttpClient.Builder builder) {
         return from(builder.build());
     }
 
+    /**
+     * This one should only used in unit tests.
+     *
+     * Please consider using the no arg build() anv set timeout values with env / system property variables.
+     *
+     * @param client
+     * @return
+     */
+    @Deprecated
     static public OkHttpClientWrapper from(OkHttpClient client) {
         return new OkHttpClientWrapper(client);
     }
