0dcecefd7a66fcae53d909c748cbf99149c6e0bc
nicolas@flexio.fr <nicolas@flexio.fr>
2018-09-20 17:38:07 +0200

    Fix test

--- cdm-rest-api-client-generator-php/src/main/java/org/codingmatters/rest/php/api/client/Main.java
+++ cdm-rest-api-client-generator-php/src/main/java/org/codingmatters/rest/php/api/client/Main.java
@@ -28,8 +28,8 @@ public class Main {
 
             PhpClientRequesterGenerator requesterGenerator = new PhpClientRequesterGenerator( clientPackage, apiPackage, typesPackage, rootDir, useReturnType );
 
-            RamlModelResult model = new RamlModelBuilder().buildApi( "/home/nico/workspace/codingmatters-rest/cdm-rest-api-client-generator-php/src/test/resources/alreadyDefinedType.raml" );
-//            RamlModelResult model = new RamlModelBuilder().buildApi( rootDir.getPath() + "/test.raml" );
+//            RamlModelResult model = new RamlModelBuilder().buildApi( "/home/nico/workspace/codingmatters-rest/cdm-rest-api-client-generator-php/src/test/resources/alreadyDefinedType.raml" );
+            RamlModelResult model = new RamlModelBuilder().buildApi( rootDir.getPath() + "/test.raml" );
 
             System.out.println( "Generating api in " + rootDir );
 
