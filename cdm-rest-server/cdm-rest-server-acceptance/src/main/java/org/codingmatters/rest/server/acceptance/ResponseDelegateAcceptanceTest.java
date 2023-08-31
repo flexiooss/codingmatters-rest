4eeab882b010dc8c2f6429f20e5cbad50051ad25
nel <nel@flexio.fr>
2023-08-31 17:45:40 +0200

    more acceptance tests
    ref #126

--- cdm-rest-server/cdm-rest-server-acceptance/src/main/java/org/codingmatters/rest/server/acceptance/ResponseDelegateAcceptanceTest.java
+++ cdm-rest-server/cdm-rest-server-acceptance/src/main/java/org/codingmatters/rest/server/acceptance/ResponseDelegateAcceptanceTest.java
@@ -4,6 +4,8 @@ import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import org.junit.Test;
 
+import java.io.*;
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -39,9 +41,96 @@ public abstract class ResponseDelegateAcceptanceTest extends BaseAcceptanceTest 
     }
 
     @Test
-    public void payload() throws Exception {
+    public void givenHeaderSet__whenAddingIfNot__thenHeaderUntouched() throws Exception {
+
+        this.withProcessor((requestDeleguate, responseDeleguate) -> {
+            responseDeleguate
+                .addHeader("header", "existing value");
+            responseDeleguate
+                .addHeaderIfNot( "header", "new value" );
+        });
+
+        assertThat(this.client.newCall(this.requestBuilder().get().build()).execute().header("header"), is("existing value"));
+    }
+
+    @Test
+    public void givenHeaderNotSet__whenAddingIfNot__thenHeaderSet() throws Exception {
+
+        this.withProcessor((requestDeleguate, responseDeleguate) -> {
+            responseDeleguate
+                .addHeaderIfNot( "header", "new value" );
+        });
+
+        assertThat(this.client.newCall(this.requestBuilder().get().build()).execute().header("header"), is("new value"));
+    }
+
+    @Test
+    public void whenPayloadAsString() throws Exception {
         this.withProcessor((requestDeleguate, responseDeleguate) -> {responseDeleguate.payload("yop yop", "utf-8");});
 
         assertThat(this.client.newCall(this.requestBuilder().get().build()).execute().body().string(), is("yop yop"));
+    }
+
+    @Test
+    public void whenPayloadAsBytes() throws Exception {
+        this.withProcessor((requestDeleguate, responseDeleguate) -> {responseDeleguate.payload("yop yop".getBytes("utf-8"));});
+
+        assertThat(this.client.newCall(this.requestBuilder().get().build()).execute().body().string(), is("yop yop"));
+    }
+
+
+
+    @Test
+    public void whenPayloadAsStream() throws Exception {
+        this.withProcessor((requestDeleguate, responseDeleguate) -> {
+            try(InputStream in = new ByteArrayInputStream("yop yop".getBytes("utf-8"))) {
+                responseDeleguate.payload(in);
+            }
+        });
+
+        assertThat(this.client.newCall(this.requestBuilder().get().build()).execute().body().string(), is("yop yop"));
+    }
+
+    @Test
+    public void whenPayloadIsABigStream() throws Exception {
+        File f = null;
+        try {
+            f = this.generateBigFile();
+            File file = f;
+            this.withProcessor((requestDeleguate, responseDeleguate) -> {
+                try (InputStream in = new FileInputStream(file)) {
+                    responseDeleguate.payload(in);
+                }
+            });
+
+            assertThat(this.client.newCall(this.requestBuilder().get().build()).execute().body().string(), is(this.content(f)));
+        } finally {
+            if(f != null) {
+                f.delete();
+            }
+        }
+    }
+
+    private String content(File file) throws IOException {
+        try(ByteArrayOutputStream result = new ByteArrayOutputStream() ; InputStream in = new FileInputStream(file)) {
+            byte[] buffer = new byte[1024];
+            for(int read = in.read(buffer); read != -1 ; read = in.read(buffer)) {
+                result.write(buffer, 0, read);
+            }
+            result.flush();
+            result.close();
+            return new String(result.toByteArray(), "utf-8");
+        }
+    }
+
+    private File generateBigFile() throws IOException {
+        File result = File.createTempFile("bif-file", ".txt");
+        try(OutputStream out = new FileOutputStream(result)) {
+            for (int i = 0; i < 100000; i++) {
+                out.write("azertyuiopqsdfghjklmwxcvbn\n".getBytes("utf-8"));
+            }
+            out.flush();
+        }
+        return result;
     }
 }
